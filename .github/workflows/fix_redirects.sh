d2844a02ae1ad4bd214f4d2320210af1bf82231c
Peter <magic@kthx.at>
2022-02-13 14:10:14 +0100

    test

--- .github/workflows/fix_redirects.sh
+++ .github/workflows/fix_redirects.sh
@@ -1,7 +1,5 @@
 #/bin/bash
-cd ../../ # go in docsdir
-ls -lah
-find ./site/ -maxdepth 2 -mindepth 2 -type f -name 'index.html' -not -path './site/de/*' -not -path './site/en/*' | while read f; do
+find ../site/ -maxdepth 2 -mindepth 2 -type f -name 'index.html' -not -path '../site/de/*' -not -path '../site/en/*' | while read f; do
 	echo "Fixing redirect '$f'..."
 	sed -i -E 's/\.en\/"/\/"/' $f
 done
