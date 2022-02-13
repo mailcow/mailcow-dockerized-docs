dca509d7cdf5dab7cb575966508c9cfb799325f5
Peter <magic@kthx.at>
2022-02-13 14:15:27 +0100

    test

--- .github/workflows/fix_redirects.sh
+++ .github/workflows/fix_redirects.sh
@@ -1,7 +1,4 @@
 #/bin/bash
-ls . -lah
-ls ../ -lah
-ls ../../ -lah
 find ./site/ -maxdepth 2 -mindepth 2 -type f -name 'index.html' -not -path './site/de/*' -not -path './site/en/*' | while read f; do
 	echo "Fixing redirect '$f'..."
 	sed -i -E 's/\.en\/"/\/"/' $f
